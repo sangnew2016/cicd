39c77c3e108d230d8d6ce3f5bef4716b28c304e3
sang-thach-intec <sang.intec@gmail.com>
2020-11-03 15:19:36 +0700

    Commit: 2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,8 +55,10 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+import 'intl';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
